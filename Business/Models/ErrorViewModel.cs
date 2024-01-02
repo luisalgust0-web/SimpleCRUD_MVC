32b0252d99a60cf552c00fd47402d28722ce7c8d
luisPC <luisalgustonunesdemoraes@gmail.com>
2024-01-02 20:08:21 -0300

    Add project files.

---
+++ Business/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SimpleCRUD_MVC.Business.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
